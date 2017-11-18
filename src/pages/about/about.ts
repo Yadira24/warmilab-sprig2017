fa2ab2482f6a656051027bd3810e21fcd1ec37f2
Arturo Satoshi Quispe <yadira_961@outlook.com>
2017-11-18 12:08:31 -0500

    agregar variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+  nombre: string = "Zaha Hadid";
+  ocupacion: string = "architect";
+  edad: number = 65;
+  origen: string ="Bagdad,Irak";
+
+
+    datos:any = {
+      nombre: "Zaha Hadid",
+      ocupacion: "architect",
+      edad: 65,
+      origen:"Bagdad, Irak"
+    };
+
+
   constructor(public navCtrl: NavController) {
 
   }
-
 }
